65dbe62ae89d26f23b46e4cd71f3d9ce979aaa1d
Ali Raza <ali.raza030958@gmail.com>
2023-08-25 12:04:12 +0500

    second

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-// import Revenue from "./Revenue";
 
-export default function () {
+export default function Layout() {
   return (
     <div className=" flex lg:flex-row flex-col bg-white h-screen w-screen">
       <Sidebar />
-      {/* <Revenue />    */}
+
       <div className="flex-1 ">
         <Header />
         <div className="p-4">{<Outlet />}</div>
